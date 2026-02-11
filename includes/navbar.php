e89609eecc5bc38c5316c046b3eff37d42445479
RAJAT3336 <rajatmaharjan45@gmail.com>
2026-02-11 18:08:47 +0545

    working cart numbers

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,4 +1,4 @@
-<section id="header">
+<!-- <section id="header">
     <a href="index.php"> <img src="Image/Logo/without-bg.png" alt="logo" class="logo" height="70px" width="70px"></a>
 
     <div>
@@ -16,10 +16,69 @@
             <li>
                 <a href="cart.php" class="cart-icon">
                     <i class="fa-solid fa-cart-shopping"></i><span id="card-item">1</span>
-                    <!-- <span class="cart-item-count"></span> -->
-                </a>
+                    <span class="cart-item-count"></span>
+                 </a>
             </li>
             <li><a href=""><i class="fa-solid fa-user"></i></a></li>
+
+        </ul>
+    </div>
+</section> -->
+
+<section id="header">
+    <a href="index.php">
+        <img src="Image/Logo/without-bg.png" alt="logo" class="logo" height="70px" width="70px">
+    </a>
+
+    <div>
+        <ul id="navbar">
+            <li><a href="#">Mens</a></li>
+            <li><a href="#">Womens</a></li>
+            <li><a href="#">New Arrivals</a></li>
+            <li><a href="#">Seasonal</a></li>
+            <li>
+                <div class="box">
+                    <input type="text" placeholder="Search...">
+                    <a href="#"><i class="fa-solid fa-magnifying-glass"></i></a>
+                </div>
+            </li>
+
+            <li>
+                <a href="cart.php" class="cart-icon">
+                    <i class="fa-solid fa-cart-shopping"></i>
+                    <span id="card-item">
+                        <?php
+                        if (isset($_SESSION['auth'])) {
+                            $uid = $_SESSION['loggedInUser']['user_id'];
+                            $cart_query = "SELECT * FROM cart WHERE user_id = '$uid'";
+                            $cart_query_run = mysqli_query($conn, $cart_query);
+
+                            if ($cart_query_run) {
+                                echo mysqli_num_rows($cart_query_run);
+                            } else {
+                                echo "0";
+                            }
+                        } else {
+                            echo "0";
+                        }
+                        ?>
+                    </span>
+                </a>
+            </li>
+
+            <li>
+                <?php if (isset($_SESSION['auth'])): ?>
+                    <div class="nav-user-info" style="display: inline-flex; align-items: center; gap: 8px;">
+                        <a href="profile.php" style="color: #7a1e2c; font-weight: 600;">
+                            <i class="fa-solid fa-user"></i>
+                            <?= explode(' ', trim($_SESSION['loggedInUser']['name']))[0]; ?>
+                        </a>
+                        <a href="logout.php" style="font-size: 12px; color: #666;">(Logout)</a>
+                    </div>
+                <?php else: ?>
+                    <a href="login.php"><i class="fa-solid fa-user"></i></a>
+                <?php endif; ?>
+            </li>
 
         </ul>
     </div>
